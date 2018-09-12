14286c7351dd1909a98e668219cbf1846ce9e229
Ayesha Perwaiz <43158079+ayeshaperwaiz@users.noreply.github.com>
2018-09-11 21:49:44 -0600

    Update unsortedVector.java

--- unsortedVector.java
+++ unsortedVector.java
@@ -6,7 +6,9 @@ import java.io.FileNotFoundException; import java.util.Scanner; import java.util
  
 public class unsortedVector 
 { 
-    //stores number of assignments and comparisons     static long assignments = 0;     static long comparisons = 0; 
+    //stores number of assignments and comparisons     
+    static long assignments = 0;     
+    static long comparisons = 0; 
  
     public static void main(String args[]) 
     { 
@@ -14,7 +16,9 @@ public class unsortedVector
  
         Vector<String> readWords = new Vector<String>(); // stores words from file 
         //reads words to file 
-        readWords = readFileToVector(fileName);         Scanner sc = new Scanner(System.in);         sc.close(); 
+        readWords = readFileToVector(fileName);         
+        Scanner sc = new Scanner(System.in);         
+        sc.close(); 
  
         int uniqueWords = 0; 
  
@@ -22,7 +26,8 @@ public class unsortedVector
  
         //prints out the number of assignments, comparisons, and unique words 
         System.out.println("Number of unique words = " + uniqueWords); 
-        System.out.println("Number of assignments = " + assignments);         System.out.println("Number of comparisons = " + comparisons); 
+        System.out.println("Number of assignments = " + assignments);         
+        System.out.println("Number of comparisons = " + comparisons); 
  
     } 
  
@@ -33,10 +38,12 @@ public class unsortedVector
         for (int i=0; i < readWords.size(); i++) 
         { 
             if (readWords.get(i).equals(str)) 
-                count ++; // counts how many times the word appears             comparisons++; 
+                count ++; // counts how many times the word appears             
+                comparisons++; 
         } 
         return count; 
     }  
+ 
     public static int unsortedUniqueCount(Vector<String> readWords) 
     { 
         Vector<String> uniqueWords = new Vector<String>(); // stores all unique words 
@@ -47,9 +54,11 @@ public class unsortedVector
             if (!uniqueWords.contains(readWords.get(i))) 
             { 
                 uniqueWords.add(readWords.get(i)); 
-                uniqueCount.add(stringCount(readWords.get(i), readWords));                 assignments++; //increment the number of assignments made 
+                uniqueCount.add(stringCount(readWords.get(i), readWords));                 
+                assignments++; //increment the number of assignments made 
             } 
-            //comparisons made of all words against each other             comparisons += uniqueWords.size(); 
+            //comparisons made of all words against each other             
+            comparisons += uniqueWords.size(); 
          } 
         return uniqueWords.size(); 
     } 
@@ -61,11 +70,18 @@ public class unsortedVector
         File file = new File(fileName); 
  
         try{ 
-            Scanner sc = new Scanner(file);             while (sc.hasNextLine()) 
+            Scanner sc = new Scanner(file);             
+            while (sc.hasNextLine()) 
             { 
-                String data = sc.nextLine().toLowerCase(); //converts all characters to lowercase                 char ch=' ';                 if (!data.equals("")) 
-                    ch = data.charAt(0); // get first character of string                 // adds values to words vector if character is alphabetical.                 if (ch >= 'a' && ch <= 'z')                     words.add(data); 
-            }             sc.close(); 
+                String data = sc.nextLine().toLowerCase(); //converts all characters to lowercase                 
+                char ch=' ';                 
+                if (!data.equals("")) 
+                    ch = data.charAt(0); // get first character of string                 
+             // adds values to words vector if character is alphabetical.                 
+             if (ch >= 'a' && ch <= 'z')                     
+              words.add(data); 
+            }             
+         sc.close(); 
         } 
         catch (FileNotFoundException e) 
         { 
